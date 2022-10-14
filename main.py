74e65fe72f9593b8fc201027fdf4dc037deb1f4c
effieguenther <effiegguenther@gmail.com>
2022-10-14 00:59:48 -0400

    cell class defined

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ class Window:
 
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
+    
+    def draw_cell(self, cell, fill_color):
+        cell.draw(self.__canvas, fill_color)
 
 class Point:
     def __init__(self, x, y):
@@ -45,12 +48,63 @@ class Line:
             width=2
         )
         canvas.pack()
-    
+
+class Cell:
+    def __init__(self, point1, point2):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = point1.x
+        self.__x2 = point2.x
+        self.__y1 = point1.y
+        self.__y2 = point2.y
+
+    def draw(self, canvas, fill_color):
+        if self.has_left_wall:
+            canvas.create_line(
+            self.__x1, 
+            self.__y2, 
+            self.__x1, 
+            self.__y1, 
+            fill=fill_color, 
+            width=2
+            )
+            canvas.pack()
+        if self.has_right_wall:
+            canvas.create_line(
+            self.__x2, 
+            self.__y2, 
+            self.__x2, 
+            self.__y1, 
+            fill=fill_color, 
+            width=2
+            )
+            canvas.pack()
+        if self.has_bottom_wall:
+            canvas.create_line(
+            self.__x1, 
+            self.__y2, 
+            self.__x2, 
+            self.__y2, 
+            fill=fill_color, 
+            width=2
+            )
+            canvas.pack()
+        if self.has_top_wall:
+            canvas.create_line(
+            self.__x1, 
+            self.__y1, 
+            self.__x2, 
+            self.__y1, 
+            fill=fill_color, 
+            width=2
+            )
+            canvas.pack()            
+
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(400, 300), Point(700, 200))
-    win.draw_line(line, 'red')
     win.wait_for_close()
 
 main()
